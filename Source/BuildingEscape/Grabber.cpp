48c288c6b54fdfeaf80281ce252ed1a90e93d3e0
Silvano Cerza <silvanocerza@gmail.com>
2018-07-29 18:26:10 +0200

    Added PhysicsHandle Component to DefaultPawn_BP and check if exists in Grabber Component

--- Source/BuildingEscape/Grabber.cpp
+++ Source/BuildingEscape/Grabber.cpp
@@ -23,6 +23,17 @@ void UGrabber::BeginPlay()
 	Super::BeginPlay();
 
 	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty!"));
+
+	PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+
+	if (PhysicsHandle)
+	{
+
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("PhysicsHandleComponent for %s not found"), *GetOwner()->GetName());
+	}
 }
 
 
